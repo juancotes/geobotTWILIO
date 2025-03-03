99419f89a8ee5a391b53e34f449569e6b49d4646
juancotes <juan.cotes@urosario.edu.co>
2025-03-02 19:39:20 -0500

    Subiendo archivos del bot

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=["POST"])
+def sms_reply():
+    """ Responde a los mensajes recibidos con 'mensaje recibido' """
+    incoming_msg = request.form.get("Body")
+    
+    response = MessagingResponse()
+    response.message(f"mensaje recibido: {incoming_msg}")
+    
+    return str(response)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
